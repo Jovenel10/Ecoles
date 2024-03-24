27042302152565d2d8c38a2887f5805e6485d074
THEJOJOBOSS <jovenel.alexandre@icloud.com>
2024-03-24 17:45:42 -0400

    Ajout de la fonctionnalite superadmin

--- ecoles/ecole/admin.py
+++ ecoles/ecole/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Etudiants
 
-# Register your models here.
 class affichageTable(admin.ModelAdmin):
-    liste_display = ['Id', 'Prenom', 'Nom', 'Pays','Adresse',' Telephone']
-admin.site.register(Etudiants,affichageTable)
+    list_display = ('id', 'prenom', 'nom', 'pays', 'adresse', 'telephone')  # Assurez-vous que les noms des champs correspondent aux attributs de votre modèle Etudiants
+
+admin.site.register(Etudiants, affichageTable)
